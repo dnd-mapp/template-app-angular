35b2f65c2721db1a5dd3af896201c1768daa55f7
Oscar Wellner <oscar.wellner@dndmapp.nl.eu.org>
2026-07-31 21:58:03 +0200

    chore: add ESLint with angular-eslint and enable it in CI
    Wires up eslint.config.mjs (typescript-eslint + angular-eslint,
    prettier-compatible), adds the ng lint builder, runs eslint via
    lint-staged, and re-enables the previously TODO'd CI lint step.

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import eslint from '@eslint/js';
+import angular from 'angular-eslint';
+import prettier from 'eslint-config-prettier';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+    {
+        files: ['**/*.ts'],
+        extends: [
+            eslint.configs.recommended,
+            tseslint.configs.recommendedTypeChecked,
+            tseslint.configs.stylisticTypeChecked,
+            angular.configs.tsRecommended,
+        ],
+        processor: angular.processInlineTemplates,
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+        },
+        rules: {
+            '@angular-eslint/directive-selector': [
+                'error',
+                {
+                    type: 'attribute',
+                    prefix: 'app',
+                    style: 'camelCase',
+                },
+            ],
+            '@angular-eslint/component-selector': [
+                'error',
+                {
+                    type: 'element',
+                    prefix: 'app',
+                    style: 'kebab-case',
+                },
+            ],
+        },
+    },
+    {
+        files: ['**/*.html'],
+        extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+        rules: {},
+    },
+    prettier,
+]);
